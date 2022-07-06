2928073bef0f865a4e6790d415a79cd4fe2e0831
Suraj Khedkar <68546999+Suraj-Khedkar@users.noreply.github.com>
2022-07-06 13:22:53 +0530

    server.listen removed

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const http = require('http');
 const express = require('express');
-const path = require('path');const app = express();
-
+const path = require('path');
+const app = express();
+// const http = require('http');
 
 app.use(express.json());
 app.use(express.static("express"));// default URL for website
-app.use('/', function(req,res){
+
+
+app.get('/', function(req,res){
     res.sendFile(path.join(__dirname+'/express/index.html'));
     //__dirname : It will resolve to your project folder.
-  });const server = http.createServer(app);
-
-
+}); 
+// const server = http.createServer(app);
 const port =  process.env.port || 3000;
-server.listen(port);console.debug('Server listening on port ' + port);
+// server.listen(port);console.debug('Server listening on port ' + port);
+app.listen(port, () => console.log(`app listening on port ${port}!`));
